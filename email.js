b370c62ae1601c18d33e9c9c8b548284eccf1d33
Nisharma18 <ns160200@gmail.com>
2024-07-22 00:38:56 +0530

    update contact form

--- email.js
+++ email.js
@@ -1,18 +1,26 @@
 function EmailSend(){
+    const name = document.getElementById("name").value
+    const userEmail = document.getElementById("email").value
+    const message = document.getElementById("message").value
+    const subject = document.getElementById("subject").value
+
+    const formBody = `
+    name : ${name} </br>
+    email : ${userEmail} </br>
+    message: ${message}
+    `
+
     Email.send({
-        SecureToken : "C973D7AD-F097-4B95-91F4-40ABC5567812",
-        To : 'nishasharmacon@gmail.com',
-        From :  document.getElementById("email").value,
-        Subject : "This is the subject",
-        Body : document.getElementById("name").value +
-               document.getElementById("email").value +
-               document.getElementById("Subject").value +
-               document.getElementById("text").value,
-        Attachments : [
-        {
-            name : "smtpjs.png",
-            path : "https://networkprogramming.files.wordpress.com/2017/11/smtpjs.png"
-        }]
+        // SecureToken : "C973D7AD-F097-4B95-91F4-40ABC5567812",
+        Host: "smtp.elasticemail.com",
+        Username: "ns1660200@gmail.com",
+        Password: "61A4686AA6B1C5AA0D74986FEAA595D32FE8",
+        To : 'ns1660200@gmail.com',
+        From :  "ns1660200@gmail.com",
+        // From :  document.getElementById("email").value,
+        Subject : "Portfolio Enquery form : "`${subject}`,
+        Body : formBody,
+       
     }).then(
     message => alert(message)
     );
